7def76b67cdff1022b8a909f3548aca091253bce
gi-taka <worldhistory14@gmail.com>
2021-06-04 22:32:23 +0900

    sign_up.html.erbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'rooms#index'
+  get '/users/sign_up', to: 'users#sign_up'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :rooms
