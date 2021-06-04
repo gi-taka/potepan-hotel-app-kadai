9bd7b7c96705338e96095ac53e0183f4608d41f7
gi-taka <worldhistory14@gmail.com>
2021-06-04 22:58:42 +0900

    コントローラーのアクションを追加する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+
   def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def sign_in
+  end
+
+  def sign_up
   end
 end
