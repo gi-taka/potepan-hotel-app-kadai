e0c742a38d8db5c977e54246017aada397def424
gi-taka <worldhistory14@gmail.com>
2021-06-06 22:23:02 +0900

    パスワードの暗号化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true
   validates :mail, presence: true
   validates :password, presence: true, length: {minimum: 8}, confirmation: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_125356) do
+ActiveRecord::Schema.define(version: 2021_06_06_131817) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "user_id"
@@ -37,8 +37,7 @@ ActiveRecord::Schema.define(version: 2021_06_06_125356) do
     t.string "icon_img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
-    t.string "password_confirmation"
+    t.string "password_digest"
   end
 
 end
