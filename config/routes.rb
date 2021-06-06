ea33cf5c407441442515973582c679dab7b3f90e
gi-taka <worldhistory14@gmail.com>
2021-06-06 18:29:17 +0900

    登録画面のviewをnew.html.erbに変更する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'rooms#index'
-  get '/users/sign_up', to: 'users#sign_up'
+  get '/users/sign_up', to: 'users#new'
   get '/users/sign_in', to: 'users#sign_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
