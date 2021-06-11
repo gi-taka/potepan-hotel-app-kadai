c2d4a6f8c27c2999dabd54dbc9e3e2a004c27c69
gi-taka <worldhistory14@gmail.com>
2021-06-11 22:34:32 +0900

    user/account.html.erbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'users/sign_up', to: 'users#new'
   post 'users/login', to: 'users#login'
   post 'users/logout', to: 'users#logout'
+  get 'users/account', to: 'users#account'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :rooms
