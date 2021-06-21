7b85cef7a09cc162441a176d77f2fba0f70b992c
gi-taka <worldhistory14@gmail.com>
2021-06-21 23:16:48 +0900

    検索結果画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'users/account', to: 'users#account'
   get 'users/profile', to: 'users#profile'
   patch 'users/:id/profile_update', to: 'users#profile_update'
+  get 'rooms/search', to: 'rooms#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :rooms
