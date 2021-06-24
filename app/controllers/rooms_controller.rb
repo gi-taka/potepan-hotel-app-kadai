a99f063a3871080684eeacd086f6e3812766a20a
gi-taka <worldhistory14@gmail.com>
2021-06-24 23:11:15 +0900

    ルーム登録機能を作る

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,9 +6,16 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(params.require(:room).permit(:name, :room_introduction, :fee, :address, :image))
+    if @room.save
+      redirect_to "/"
+    else
+      render "/rooms/new"
+    end
   end
 
   def edit
