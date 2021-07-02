ecb8a0f3583adf024bf5448740465df33a417ae1
gi-taka <worldhistory14@gmail.com>
2021-07-02 22:25:39 +0900

    予約機能の作成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,6 +6,7 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_141801) do
+ActiveRecord::Schema.define(version: 2021_07_02_132253) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "person_num"
+    t.integer "total_amount"
   end
 
   create_table "rooms", force: :cascade do |t|
